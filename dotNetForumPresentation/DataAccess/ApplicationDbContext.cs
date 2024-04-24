ec193cf0d02ec2966723a161a7daece0f1704fb2
Louis van Zyl <louisvanzyl@outlook.com>
2024-04-24 08:50:05 +0200

    Set up base db context

---
+++ dotNetForumPresentation/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarMechanic> CarMechanics { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Engine> Engines { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
+    }
+}
