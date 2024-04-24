2d32aaeab9cd6a4ab069336f1f5a8e7b1ffb7732
Louis van Zyl <louisvanzyl@outlook.com>
2024-04-24 09:08:05 +0200

    Add sqlite connection

--- dotNetForumPresentation/DataAccess/ApplicationDbContext.cs
+++ dotNetForumPresentation/DataAccess/ApplicationDbContext.cs
@@ -11,5 +11,10 @@ namespace DataAccess
         public DbSet<Engine> Engines { get; set; }
         public DbSet<Manufacturer> Manufacturers { get; set; }
         public DbSet<Mechanic> Mechanics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ dotNetForumPresentation/Fixtures/SqlLiteConnection.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fixtures
+{
+    public abstract class SqlLiteConnection: IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly ApplicationDbContext _dbContext;
+
+        protected SqlLiteConnection()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            var context = new ApplicationDbContext(options);
+            context.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
